2a6bd06ed06ed45d57bb61fd589f716c0cf9c981
demiler <seerpout@gmail.com>
2021-03-22 16:39:49 +0300

    app.js changes: moved create window to separate file. Renamed class to TOSCapp. Added window switch

--- src/js/app.js
+++ src/js/app.js
@@ -1,129 +1,95 @@
 import { LitElement, css, html } from 'lit-element'
-import './tosc-scroll.js'
-import './tosc-button.js'
-
+import './tosc-list.js'
+import './tosc-create.js'
+import { TOSC } from './tosc.js'
 
 const pronouns = [ 'ze/hir', 'ze/zir', 'she', 'he', 'they/them/themselves',
   'they/them/themself', 'xey', 'sie', 'it', 'ey', 'e', 'hu', 'peh', 'per', 'thon' ];
 
-class TOSC extends LitElement {
+const examples = [
+  { name: "Example 0", pronoun: "xey", tosc: new TOSC("RG+B+B+") },
+  { name: "Example 1", pronoun: "sie", tosc: new TOSC("RGBB") },
+  { name: "Example 2", pronoun: "he", tosc: new TOSC("RR+GG") },
+  { name: "Example 3", pronoun: "they/them/themselves", tosc: new TOSC("G+G+GG+") },
+  { name: "Example 4", pronoun: "", tosc: new TOSC("RBGR+") },
+  { name: "Example 5", pronoun: "sie", tosc: new TOSC("G+RGG+") },
+  { name: "Example 6", pronoun: "", tosc: new TOSC("R+G+G+G+") },
+  { name: "Example 7", pronoun: "xey", tosc: new TOSC("B+R+G+G") },
+  { name: "Example 8", pronoun: "ze/hir", tosc: new TOSC("B+RGG") },
+  { name: "Example 9", pronoun: "she", tosc: new TOSC("RBB+B") },
+];
+
+
+class TOSCapp extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-
-      }
-
-      .container {
-        background-color: #232329;
-        border-radius: 10px;
-
-        width: 60%;
-        height: 90%;
-      }
-
-      .name {
-        background: transparent;
-        border: none;
-        border-bottom: 2px solid pink;
-        color: #eee;
-        margin-bottom: 10px;
         display: block;
-      }
-
-      .pronoun {
-        background: #aaaaaa5a;
-        padding: 3px;
-        border-radius: 5px;
-        display: inline-block;
-        margin-bottom: 20px;
-      }
-
-      .hint {
-        text-align: center;
-        font-size: 50px;
-      }
-
-      .controls {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
         width: 500px;
+        height: 800px;
         margin: auto;
       }
 
-      .scrolls, .extra {
-        display: flex;
+      .container {
+        /*background-color: #444569;*/
+        background-color: #1e1e2e;
+        border-radius: 10px;
+
         width: 100%;
-        justify-content: space-between;
+        height: 100%;
+        padding: 5px;
       }
 
-      tosc-scroll {
-        height: 600px;
+      tosc-list {
       }
-
-      tosc-button {
-        width: 100px;
-        height: 50px;
-      }
-
     `;
   }
 
   static get properties() {
     return {
+      showList: { type: Boolean },
     };
   }
 
   constructor() {
     super();
-    this.name = 'demiler';
-    this.pronoun = 'he';
-    this.hint = 'This is the hint';
-    this.tosk = {
-      "T": ["blue", false],
-      "O": ["blue", false],
-      "S": ["red", true],
-      "K": ["green", true],
+    this.me = {
+      name: 'Guest32432989',
+      pronoun: 'he',
+      tosc: new TOSC("R+RBG+")
     };
+    this.hint = '';
+    //this.showList = true;
+    this.showList = false;
   }
 
   render() {
+    return this.showList ? this.renderList() : this.renderCreate();
+  }
+
+  renderList() {
     return html`
       <div class='container'>
-        <div class='personal'>
-          <input class='name' value=${this.name}>
-          <div class='pronoun'>${this.pronoun}</div>
-        </div>
-
-        <div class='hint'>${this.hint}</div>
-
-        <div class='controls'>
-          <div class='scrolls'>
-            ${["T", "O", "S", "K"].map(el => html`
-              <tosc-scroll .active=${this.tosk[el][0]} .extra=${this.tosk[el][1]}
-                .letter=${el}></tosc-scroll>
-            `)}
-          </div>
-          <div class='extra'>
-            ${["T", "O", "S", "K"].map(el => html`
-              <tosc-button .letter=${el} .state=${this.tosk[el][1]}
-                @toggle=${this.toggleExtra}></tosc-button>
-            `)}
-          </div>
-        </div>
+        <tosc-list .me=${this.me} .list=${examples}
+        @switch=${this.changeScreen}
+        ></tosc-list>
       </div>
     `;
   }
 
-  toggleExtra(e) {
-    this.tosk[e.detail.letter][1] = e.detail.state;
-    this.requestUpdate();
+  renderCreate() {
+    return html`
+      <div class='container'>
+        <tosc-create .me=${this.me}
+        @button=${this.changeScreen}
+        @update=${this.updateMe}></tosc-create>
+      </div>
+    `;
+  }
+
+  changeScreen() {
+    this.showList = !this.showList;
   }
 };
 
-customElements.define('tosc-app', TOSC);
+customElements.define('tosc-app', TOSCapp);
---
+++ src/js/tosc-create.js
@@ -0,0 +1,177 @@
+import { LitElement, css, html } from 'lit-element'
+import { hints } from './hints.js'
+import { TOSC } from './tosc.js'
+import './tosc-button.js'
+import './tosc-scroll.js'
+
+class TOSCcreate extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: grid;
+        height: 100%;
+        width: 100%;
+        grid-template-rows: 100px 80px auto;
+      }
+
+      .name {
+        background: transparent;
+        border: none;
+        border-bottom: 2px solid silver;
+        color: #eee;
+        display: block;
+
+        font-size: 25px;
+        width: calc(100% - 20px);
+        margin: 20px 10px;
+
+        outline: none;
+      }
+
+      .name:focus-visible {
+        border-color: pink;
+      }
+
+      .pronoun {
+        background: #aaaaaa5a;
+        padding: 3px;
+        border-radius: 5px;
+        display: inline-block;
+        margin-bottom: 20px;
+      }
+
+      .hint {
+        text-align: center;
+        font-size: 3vw;
+        width: 100%;
+        height: 80px;
+      }
+
+      .controls {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 500px;
+        height: 100%;
+        margin: auto;
+      }
+
+      .scrolls, .extra {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+      }
+
+      .scrolls {
+        /*background-color: rgba(0, 0, 0, 0.37);*/
+        border-radius: 5px;
+        box-sizing: border-box;
+        padding: 8px;
+        margin-bottom: 20px;
+      }
+
+      tosc-scroll {
+        height: 400px;
+      }
+
+      tosc-button {
+        width: 100px;
+        height: 50px;
+      }
+
+      .btn {
+        margin-top: auto;
+        margin-bottom: 20px;
+        width: 100px;
+        height: 40px;
+
+        border: none;
+        border-radius: 5px;
+        background-color: #555555;
+        color: #eee;
+        font-size: 20px;
+        outline: none;
+      }
+
+      .btn:focus-visible,
+      .btn:hover {
+        background-color: #666555;
+      }
+
+      .btn:active {
+        filter: brightness(1.2);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      hint: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class='personal'>
+        <input class='name' value=${this.me.name} spellcheck="false"
+          @change=${this.changeName}>
+        <div class='pronoun'>${this.me.pronoun}</div>
+      </div>
+
+      <div class='hint'>${this.hint}</div>
+
+      <div class='controls'>
+        <div class='scrolls'>
+          ${["T", "O", "S", "C"].map(el => html`
+            <tosc-scroll
+            .active=${this.me.tosc[el].color} .extra=${this.me.tosc[el].extra}
+            .letter=${el} @update=${this.showHint}></tosc-scroll>
+          `)}
+        </div>
+        <div class='extra'>
+          ${["T", "O", "S", "C"].map(el => html`
+            <tosc-button .letter=${el} .state=${this.me.tosc[el].extra}
+              @toggle=${this.toggleExtra}></tosc-button>
+          `)}
+        </div>
+
+        <button class='btn' @click=${this.buttonClick}>Go back</button>
+      </div>
+    `;
+  }
+
+  changeName(e) {
+    this.me.name = e.currentTarget.value;
+  }
+
+  updateHint(letter, color, extra) {
+    this.hint = hints[letter][color][extra | 0];
+  }
+
+  showHint(e) {
+    const data = e.detail;
+    this.me.tosc[data.letter].color = data.color;
+    this.updateHint(data.letter, data.color, data.extra);
+  }
+
+  toggleExtra(e) {
+    const letter = e.detail.letter;
+    const color = this.me.tosc[letter].color;
+    this.me.tosc[letter].extra = e.detail.state;
+    this.updateHint(letter, color, e.detail.state);
+    this.requestUpdate();
+  }
+
+  buttonClick(e) {
+    const btnClkEv = new CustomEvent('button', {
+      bubbles: true,
+      composed: true });
+    this.dispatchEvent(btnClkEv);
+  }
+};
+
+customElements.define('tosc-create', TOSCcreate);
