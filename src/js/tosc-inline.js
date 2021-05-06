747d7bf2ff1e307e89fbeb863aa33cb1f01e60e8
demiler <seerpout@gmail.com>
2021-05-07 02:11:31 +0300

    Well, not it merged

--- src/js/tosc-inline.js
+++ src/js/tosc-inline.js
@@ -2,7 +2,6 @@ import { LitElement, css, html } from 'lit-element';
 import { TOSC } from './tosc.js';
 
 class TOSCinline extends LitElement {
-<<<<<<< HEAD
     static get styles() {
         return css`
             :host {
@@ -76,103 +75,6 @@ class TOSCinline extends LitElement {
             `)}
         `;
     }
-=======
-  static get styles() {
-    return css`
-      :host {
-        --text-size: 25px;
-        --border-width: 1.5px;
-        --text-ratio: 0.8;
 
-        --red: #b02323;
-        --blue: #5523f0;
-        --green: #23b033;
-
-        display: inline-block;
-        height: 100%;
-      }
-
-      .container {
-        display: flex;
-        justify-content: space-between;
-      }
-
-      .letter {
-        font-size: calc(var(--text-ratio) * var(--text-size));
-        width: calc(var(--text-size));
-        height: calc(var(--text-size));
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-
-      .wrap {
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-
-      .circle {
-        content: '';
-        width: calc(var(--text-size));
-        height: calc(var(--text-size));
-
-        position: absolute;
-        top: calc(50% - (var(--border-widdth) + var(--text-size)) / 2);
-        left: calc(50% - (var(--border-widdth) + var(--text-size)) / 2);
-
-        border: var(--border-width) solid transparent;
-        border-radius: 50%;
-      }
-
-      .extra.red .circle {
-        border-color: var(--red);
-      }
-      .extra.blue .circle {
-        border-color: var(--blue);
-      }
-      .extra.green .circle {
-        border-color: var(--green);
-      }
-
-      .red {
-        color: var(--red);
-      }
-      .blue {
-        color: var(--blue);
-      }
-      .green {
-        color: var(--green);
-      }
-    `;
-  }
-
-  static get properties() {
-    return {};
-  }
-
-  constructor() {
-    super();
-    this.tosc = new TOSC('BBBB');
-  }
-
-  render() {
-    return html`
-      <span class="container">
-        ${['T', 'O', 'S', 'C'].map(
-          (lt) => html`
-            <span class="wrap ${this.tosc[lt].color} ${this.tosc[lt].extra ? 'extra' : ''}">
-              <span class="letter">${lt}</span>
-              <span class="circle"></span>
-            </span>
-          `
-        )}
-      </span>
-    `;
-  }
->>>>>>> 044c5df8e7aa1c3092106a860032c1056a5145d5
 }
-
 customElements.define('tosc-inline', TOSCinline);
