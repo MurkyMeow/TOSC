26a2425e803508b7a0d44545105b28c2590c2831
demiler <seerpout@gmail.com>
2021-05-06 17:34:03 +0300

    Examples moved to the separate file, added isMobile function

---
+++ src/js/example.js
@@ -0,0 +1,48 @@
+import { TOSC } from './tosc';
+
+export const examples = [
+    {
+        name: 'Example 0 really long name with short pro',
+        pronoun: 'xey',
+        tosc: new TOSC('RG+B+B+')
+    },
+    {
+        name: 'Example 1 long name',
+        pronoun: 'sie',
+        tosc: new TOSC('RGBB')
+    },
+    { name: 'Example 2', pronoun: 'he', tosc: new TOSC('RR+GG') },
+    {
+        name: 'Example 3 really long name with long pro',
+        pronoun: 'they/them/themselves',
+        tosc: new TOSC('G+G+GG+'),
+    },
+    { name: 'Example 4', pronoun: '', tosc: new TOSC('RBGR+') },
+    {
+        name: 'Example 5 long name',
+        pronoun: 'they/them/themselves',
+        tosc: new TOSC('G+RGG+')
+    },
+    { name: 'Example 6', pronoun: '', tosc: new TOSC('R+G+G+G+') },
+    { name: 'Example 7', pronoun: 'xey', tosc: new TOSC('B+R+G+G') },
+    { name: 'Example 8', pronoun: 'ze/hir', tosc: new TOSC('B+RGG') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+    { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
+];
+
