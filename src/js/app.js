7793253ff12cfe371af95a62eec9f57c0ac83eae
demiler <seerpout@gmail.com>
2021-05-06 12:04:35 +0300

    Removed container, changed indetation, added mobile style

--- src/js/app.js
+++ src/js/app.js
@@ -1,88 +1,94 @@
 import { LitElement, css, html } from 'lit-element';
-import './tosc-list.js';
-import './tosc-create.js';
-import { TOSC } from './tosc.js';
-
-const examples = [
-  { name: 'Example 0', pronoun: 'xey', tosc: new TOSC('RG+B+B+') },
-  { name: 'Example 1', pronoun: 'sie', tosc: new TOSC('RGBB') },
-  { name: 'Example 2', pronoun: 'he', tosc: new TOSC('RR+GG') },
-  {
-    name: 'Example 3',
-    pronoun: 'they/them/themselves',
-    tosc: new TOSC('G+G+GG+'),
-  },
-  { name: 'Example 4', pronoun: '', tosc: new TOSC('RBGR+') },
-  { name: 'Example 5', pronoun: 'sie', tosc: new TOSC('G+RGG+') },
-  { name: 'Example 6', pronoun: '', tosc: new TOSC('R+G+G+G+') },
-  { name: 'Example 7', pronoun: 'xey', tosc: new TOSC('B+R+G+G') },
-  { name: 'Example 8', pronoun: 'ze/hir', tosc: new TOSC('B+RGG') },
-  { name: 'Example 9', pronoun: 'she', tosc: new TOSC('RBB+B') },
-];
+import { isMobile } from './isMobile'
+import { examples } from './example'
+import { TOSC } from './tosc';
+import './tosc-list';
+import './tosc-create';
 
 class TOSCapp extends LitElement {
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-        width: 500px;
-        height: 800px;
-        margin: auto;
-      }
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+                margin: auto;
+                /*background-color: #444569;*/
+                /*background-color: #1e1e2e;*/
+                background-color: #ddd;
+                color: #222;
+                border-radius: 10px;
+                font-family: sans;
+            }
 
-      .container {
-        /*background-color: #444569;*/
-        background-color: #1e1e2e;
-        border-radius: 10px;
+            :host > * {
+                --font-size: 20pt;
+                font-size: var(--font-size);
+            }
 
-        width: 100%;
-        height: 100%;
-        padding: 5px;
-      }
-    `;
-  }
+            :host {
+                width: 100%;
+                height: 80%;
+            }
 
-  static get properties() {
-    return {
-      showList: { type: Boolean },
-    };
-  }
+            @media (hover: none) and (pointer: coarse) { /*mobile*/
+                :host {
+                    width: 100%;
+                    height: 100%;
+                }
 
-  constructor() {
-    super();
-    this.me = {
-      name: 'Guest32432989',
-      pronoun: '',
-      tosc: new TOSC('BBBB'),
-    };
-    this.hint = '';
-    //this.showList = true;
-    this.showList = false;
-  }
+                :host > * {
+                    --font-size: 30px;
+                    font-size: var(--font-size);
+                }
+            }
 
-  render() {
-    return this.showList ? this.renderList() : this.renderCreate();
-  }
+        `;
+    }
 
-  renderList() {
-    return html`
-      <div class="container">
-        <tosc-list .me=${this.me} .list=${examples} @switch=${this.changeScreen}></tosc-list>
-      </div>
-    `;
-  }
+    static get properties() {
+        return {
+            showList: { type: Boolean },
+        };
+    }
 
-  renderCreate() {
-    return html`
-      <div class="container">
-        <tosc-create .me=${this.me} @button=${this.changeScreen} @update=${this.updateMe}></tosc-create>
-      </div>
-    `;
-  }
+    constructor() {
+        super();
+        this.me = {
+            name: 'Guest32432989',
+            pronoun: '',
+            tosc: new TOSC('BBBB'),
+        };
 
-  changeScreen() {
-    this.showList = !this.showList;
-  }
+        this.isMobile = isMobile();
+        this.showList = true;
+
+        //this.showList = false;
+    }
+
+    render() {
+        if (!this.isMobile) return this.renderList();
+        return this.showList ? this.renderList() : this.renderCreate();
+    }
+
+    renderList() {
+        return html`
+            <tosc-list .me=${this.me} .list=${examples}
+                @switch=${this.changeScreen}
+            ></tosc-list>
+        `;
+    }
+
+    renderCreate() {
+        return html`
+            <tosc-create .me=${this.me}
+                @button=${this.changeScreen}
+                @update=${this.updateMe}
+            ></tosc-create>
+        `;
+    }
+
+    changeScreen() {
+        this.showList = !this.showList;
+    }
 }
 
 customElements.define('tosc-app', TOSCapp);
