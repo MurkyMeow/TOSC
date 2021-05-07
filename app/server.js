ce7b508b3892061073d1510fc68a3d9b5215ac24
demiler <seerpout@gmail.com>
2021-05-07 13:59:47 +0300

    port fixed

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.port || 9080;
+const PORT = process.env.PORT || 9080;
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'public/')));
 
