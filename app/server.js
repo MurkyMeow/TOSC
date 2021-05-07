59cc0be233f4109ca4263ab8000294b9505f67cc
demiler <seerpout@gmail.com>
2021-05-07 13:57:10 +0300

    port fixed

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.port || 9080;
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'public/')));
 
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public/', 'index.html'));
 });
 
-app.listen(9080, () => {
-  console.log('Starting server on http://localhost:9080');
+app.listen(PORT, () => {
+    console.log('Starting server on port', PORT);
 });
