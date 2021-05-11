2efaaf7c69eeb7e7e8005ad44a0dae82715e1007
MurkyMeow <dinosowermurky@gmail.com>
2021-05-11 23:59:43 +0300

    draft

--- app/server.js
+++ app/server.js
@@ -1,5 +1,8 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
+
+const { startWebSocket } = require('./websocket');
 
 const PORT = process.env.PORT || 9080;
 const app = express();
@@ -7,6 +10,11 @@ app.use(express.static(path.resolve(__dirname, 'public/')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/', 'index.html'));
+});
+
+startWebSocket({
+    server: http.createServer(app),
+    path: '/ws',
 });
 
 app.listen(PORT, () => {
